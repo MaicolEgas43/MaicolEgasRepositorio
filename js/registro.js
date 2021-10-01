2dce3a59fad54b46b1cdd362eae12227b6ab3a5d
MaicolEgas43 <megas@unal.edu.co>
2021-09-30 20:42:01 -0500

    Primer Js

--- js/registro.js
+++ js/registro.js
@@ -1,4 +1,23 @@
 function botonGuardar (){
     // Mensajes en la consola
-    console.log("Mi primer javaScript");
+    let nombre = document.getElementById("nombres").value ;
+    let apellidos = document.getElementById("apellidos").value;
+    let contrasena = document.getElementById("contrasena").value;
+
+    if(nombre === ""){
+        alert("Digite por favor el nombre")
+    }
+    if(apellidos === ""){
+        alert("Digite por favor los apellidos")
+
+    }
+    if (contrasena === ""){
+        alert("Digite por favor la contraseña")
+    }
+    
+
+    if(nombre !== "" && apellidos !== "" && contrasena!== ""){
+        alert("tus datos estan correctos")
+    }
+    console.log(nombre,apellidos);
 }
