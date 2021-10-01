92e79b40568e6c54a9dec08be0ed16a8ecd7b524
MaicolEgas43 <megas@unal.edu.co>
2021-09-30 21:15:31 -0500

    Parte final

--- js/registro.js
+++ js/registro.js
@@ -1,23 +1,44 @@
 function botonGuardar (){
-    // Mensajes en la consola
-    let nombre = document.getElementById("nombres").value ;
-    let apellidos = document.getElementById("apellidos").value;
-    let contrasena = document.getElementById("contrasena").value;
 
-    if(nombre === ""){
+    let nombreEl = document.getElementById("nombres");
+    let apellidosEl = document.getElementById("apellido");
+    let contrasenaEl = document.getElementById("contrasena")
+
+    nombreEl = classList.add("")
+
+
+    
+    let nombre = nombreEl.value ;
+    let apellidos = apellidosEl.value;
+    let contrasena = contrasenaEl.value;
+
+    // Mensajes en la consola
+
+    if(nombre === "" || nombre.length < 6 ){
         alert("Digite por favor el nombre")
+        document.getElementById("nombre").classList.add("error");
     }
     if(apellidos === ""){
-        alert("Digite por favor los apellidos")
+        alert("Digite por favor los apellidos");
+        document.getElementById("nombre").classList.add("error");
 
     }
     if (contrasena === ""){
-        alert("Digite por favor la contraseña")
+        alert("Digite por favor la contraseña");
+        document.getElementById("nombre").classList.add("error");
     }
     
 
     if(nombre !== "" && apellidos !== "" && contrasena!== ""){
-        alert("tus datos estan correctos")
+        //alert("tus datos estan correctos")
+        let datospersonale = {
+            nombre: nombre,
+            apellidos: apellidos,
+            contrasena: contrasena
+
+        }
+        console.log(datospersonale)
     }
+    
     console.log(nombre,apellidos);
 }
